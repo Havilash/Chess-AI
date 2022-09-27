d4c2e23aae6f0794b7aaf6e8419aa9ad18b5ab97
Havilash Sivaratnam <havilash2005@gmail.com>
2022-09-27 16:57:15 +0200

    feat: chess class

--- chess_game.py
+++ chess_game.py
@@ -1,14 +1,31 @@
+from tkinter.messagebox import RETRY
 import chess
 
 
 class ChessGame():
     def __init__(self) -> None:
         self.board = chess.Board()
+        # self.board = chess.Board("3k4/8/8/8/8/8/8/3K4 b KQkq - 0 4")
+
+    def is_draw(self):
+        outcome = self.board.outcome()
+        if hasattr(outcome, "winner"):
+            if outcome.winner == None:
+                return True
+        return False
+
+    def is_checkmate(self):
+        return self.board.is_checkmate()
+
+    def is_legal(self, uci):
+        return uci in self.board.legal_moves
 
     def move(self, uci):
         m = chess.Move.from_uci(uci)
-        if m in self.board.legal_moves:
-            self.board.push(m)
+        self.board.push(m)
+
+    def revert_last_move(self):
+        self.board.pop()
 
     def print_board(self):
         print(self.board)
--- main.py
+++ main.py
@@ -3,21 +3,13 @@ from chess_game import ChessGame
 
 def main():
     chess = ChessGame()
-    chess.move("a2a4")
 
-    chess.move("h7h6")
-    chess.move("a4a5")
-
-    chess.move("b7b5")
-    chess.move("a5b6")
-
-    chess.move("h6h5")
-    chess.move("b6c7")
-
-    chess.move("h5h4")
-    chess.move("c7b8")
-
-    chess.print_board()
+    is_running = True
+    while is_running:
+        chess.print_board()
+        move = input("Move: ")
+        while not chess.is_legal(move):
+            move = input("Try another move: ")
 
 
 if __name__ == "__main__":
